a430ce51676a6ccd0fabd7fe25a34b62415b5879
mdtosif <md.tosif@zeeve.io>
2024-05-16 23:02:02 +0530

    i

--- go.mod
+++ go.mod
@@ -9,12 +9,12 @@ require (
 	github.com/araddon/dateparse v0.0.0-20180729174819-cfd92a431d0e // indirect
 	github.com/fatih/set v0.2.1 // indirect
 	github.com/gigawattio/window v0.0.0-20180317192513-0f5467e35573 // indirect
-	github.com/go-resty/resty/v2 v2.0.0 // indirect
+	github.com/go-resty/resty/v2 v2.13.1 // indirect
 	github.com/jaytaylor/html2text v0.0.0-20180606194806-57d518f124b0 // indirect
 	github.com/mattn/go-runewidth v0.0.3 // indirect
 	github.com/olekukonko/tablewriter v0.0.0-20180506121414-d4647c9c7a84 // indirect
 	github.com/pkg/errors v0.8.1 // indirect
 	github.com/ssor/bom v0.0.0-20170718123548-6386211fdfcf // indirect
-	golang.org/x/net v0.24.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
 	"encoding/base64"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -12,6 +14,7 @@ import (
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/go-resty/resty/v2"
 )
 
 // Key and IV should be kept secure and unique.
@@ -22,39 +25,92 @@ var (
 	iv, _  = hex.DecodeString(ivHex)
 )
 
-func encrypt(data []byte) ([]byte, error) {
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
-	}
-
-	aesGCM, err := cipher.NewGCM(block)
-	if err != nil {
-		return nil, err
-	}
-
-	// Encrypt the data using AES-GCM with the provided IV.
-	return aesGCM.Seal(nil, iv, data, nil), nil
+// Pad the data to the block size using PKCS#7 padding
+func padData(data []byte) []byte {
+	padding := aes.BlockSize - (len(data) % aes.BlockSize)
+	padText := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(data, padText...)
 }
 
-func decrypt(encryptedData []byte) ([]byte, error) {
+func encrypt(data string) (string, error) {
+	// Convert string to byte slice
+	dataBytes := []byte(data)
+
+	key := []byte("37911490979715163134003223491201")
+	iv := []byte("3134003223491201")
+
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
-	aesGCM, err := cipher.NewGCM(block)
-	if err != nil {
-		return nil, err
+	// The IV should be the same length as the block size of the cipher
+	if len(iv) != aes.BlockSize {
+		return "", fmt.Errorf("IV length must be %d bytes", aes.BlockSize)
 	}
 
-	// Decrypt the data using AES-GCM with the provided IV.
-	return aesGCM.Open(nil, iv, encryptedData, nil)
+	// Pad the data to the block size
+	dataBytes = padData(dataBytes)
+
+	// Create a new AES cipher block mode for CBC encryption
+	mode := cipher.NewCBCEncrypter(block, iv)
+
+	// Encrypt the data
+	encryptedData := make([]byte, len(dataBytes))
+	mode.CryptBlocks(encryptedData, dataBytes)
+
+	// Encode encrypted data to base64 string
+	encodedString := base64.StdEncoding.EncodeToString(encryptedData)
+
+	return encodedString, nil
 }
 
-func main() {
+func decrypt(data string, keyNum int) (string, error) {
+	// Decode base64-encoded string to bytes
+	encryptedData, err := base64.StdEncoding.DecodeString(data)
+	if err != nil {
+		return "", err
+	}
+	key := []byte("37911490979715163134003223491201")
+	if keyNum > 1 {
+		key = []byte("54674138327930866480207815084989")
+	}
+	iv := []byte("3134003223491201")
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	if len(encryptedData) < aes.BlockSize {
+		return "", fmt.Errorf("encrypted data is too short")
+	}
+
+	// The IV should be the same length as the block size of the cipher
+	if len(iv) != aes.BlockSize {
+		return "", fmt.Errorf("IV length must be %d bytes", aes.BlockSize)
+	}
+
+	// Create a new AES cipher block mode for CBC encryption
+	mode := cipher.NewCBCDecrypter(block, iv)
+
+	// Decrypt the data
+	decryptedData := make([]byte, len(encryptedData))
+	mode.CryptBlocks(decryptedData, encryptedData)
+
+	// Remove PKCS#7 padding
+	padding := decryptedData[len(decryptedData)-1]
+	if padding > aes.BlockSize || padding == 0 {
+		return "", fmt.Errorf("invalid padding")
+	}
+	decryptedData = decryptedData[:len(decryptedData)-int(padding)]
+
+	return string(decryptedData), nil
+}
+
+func getGogocdnLink(epidoseUrl string) string {
 	// URL for fetching data
-	baseURL := "https://anitaku.so/fairy-tail-2014-episode-1"
+	baseURL := epidoseUrl //"https://anitaku.so/fairy-tail-2014-episode-1"
 
 	resp, err := http.Get(baseURL)
 	if err != nil {
@@ -70,9 +126,18 @@ func main() {
 
 	// Extract video URL
 	gogocdn := doc.Find("#load_anime > div > div > iframe").AttrOr("src", "")
-	fmt.Println("gogocdn:", strings.TrimSpace(gogocdn))
+	return strings.TrimSpace(gogocdn)
+}
 
-	resp2, err := http.Get(strings.TrimSpace(gogocdn))
+func getDecryptedData(episodeUrl string) string {
+	// Create a Resty Client
+	client := resty.New()
+
+	gogocdn := getGogocdnLink(episodeUrl)//"https://anitaku.so/sousou-no-frieren-no-mahou-episode-12"
+
+	// println(gogocdn)
+
+	resp2, err := http.Get((gogocdn))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -90,19 +155,67 @@ func main() {
 	s := doc2.Find("script[data-name='episode']")
 	scriptValue := s.AttrOr("data-value", "")
 
-	// Decode hex-encoded string to bytes
-	encryptedData, err := base64.StdEncoding.DecodeString(scriptValue)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	// Decrypt the data
-	decryptedData, err := decrypt(encryptedData)
+	decryptedData, err := decrypt(scriptValue, 1)
 	if err != nil {
 		log.Fatal("Decryption error:", err)
 	}
 
-	fmt.Println("Encrypted Data:", scriptValue)
-	fmt.Println("Decrypted Data:", string(decryptedData))
-	fmt.Println("Video ID:", videoId)
+	e, _ := encrypt(videoId)
+	// d,_ := decrypt(e)
+
+	// fmt.Println("Encrypted Data:", scriptValue, )
+	// fmt.Println("Decrypted Data:", decryptedData)
+	// fmt.Println("Video ID:", d)
+
+	newUrl, _ := url.ParseQuery(decryptedData)
+	newUrl.Set("id", e)
+	newUrl.Set("alias", videoId)
+
+	// println("url query: ", newUrl.Encode())
+
+	vidUrl := "https://" + gogoUrl.Host + "/encrypt-ajax.php?" + newUrl.Encode()
+	println(vidUrl)
+
+	http.Get(vidUrl)
+
+	resp, err := client.R().
+		SetHeader("X-Requested-With", "XMLHttpRequest").
+		Get(vidUrl)
+
+	if err != nil {
+		println("Error:", err.Error())
+	}
+
+	var respAjax struct {
+		Data string `json:"data"`
+	}
+
+	err = json.Unmarshal(resp.Body(), &respAjax)
+
+	epp, _ := decrypt(respAjax.Data, 2)
+	if err != nil {
+		println("Error:", err.Error())
+	}
+	return epp
+}
+
+func main() {
+	
+	epp := getDecryptedData("https://anitaku.so/sousou-no-frieren-no-mahou-episode-12")
+
+	var src struct {
+		Source []struct {
+			File string `json:"File"`
+		} `json:"source"`
+	}
+	err := json.Unmarshal([]byte(epp), &src)
+	if err != nil {
+		println("Error:", err.Error())
+	}
+
+	println(src.Source[0].File)
+
+	fmt.Println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+
 }
